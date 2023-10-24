7bf4a7a5fb9d849e323eedb39006e8f39c66d64e
Zdenka Plskova <plskova17@post.cz>
2023-10-24 14:18:49 +0200

    another location test

--- javascript.js
+++ javascript.js
@@ -17,46 +17,46 @@ function clearInput() {
     }
 }
 
-var button =document.getElementById('los'); 
-button.addEventListener ('click', ermittlePosition);
-var ausgabe = document.getElementById('ausgabe');
-
-function ermittlePosition() {
-  if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(zeigePosition, zeigeFehler);
-  } else { 
-      ausgabe.innerHTML = 'Ihr Browser unterstützt keine Geolocation.';
+var x = document.getElementById("demo");
+function getLocation()
+  {
+  if (navigator.geolocation)
+    {
+        navigator.geolocation.getCurrentPosition(showPosition,showError,
+          {
+            enableHighAccuracy : true,
+            timeout : 10000, // 10s
+            //maximumAge : 0
+          }
+        );
+    }
+  else{x.innerHTML="Geolocation is not supported by this browser.";}
   }
-}
-
-function zeigePosition(position) {
-  ausgabe.innerHTML = "Ihre Koordinaten sind:<br> Breite: " + position.coords.latitude + 
-  "<br>Länge: " + position.coords.longitude +'<br>Genaugikeit: ' + position.coords.accuracy;	
-}
-
-function zeigeFehler(error) {
-  switch(error.code) {
-      case error.PERMISSION_DENIED:
-          ausgabe.innerHTML = "Benutzer lehnte Standortabfrage ab."
-          break;
-      case error.POSITION_UNAVAILABLE:
-          ausgabe.innerHTML = "Standortdaten sind nicht verfügbar."
-          break;
-      case error.TIMEOUT:
-          ausgabe.innerHTML = "Die Standortabfrage dauerte zu lange (Time-out)."
-          break;
-      case error.UNKNOWN_ERROR:
-          ausgabe.innerHTML = "unbekannter Fehler."
-          break;
+function showPosition(position)
+  {
+  x.innerHTML="Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;    
   }
-}
+function showError(error)
+  {
+  switch(error.code) 
+    {
+    case error.PERMISSION_DENIED:
+      x.innerHTML="User denied the request for Geolocation."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      x.innerHTML="Location information is unavailable."
+      break;
+    case error.TIMEOUT:
+      x.innerHTML="The request to get user location timed out."
+      break;
+    case error.UNKNOWN_ERROR:
+      x.innerHTML="An unknown error occurred."
+      break;
+    }
+  }
 
- 
-
-
-
-
-// EB.Barcode.enable({}, scanReceived);
+  // EB.Barcode.enable({}, scanReceived);
        // function scanReceived(params){
             // No data or no timestamp, scan failed.
          //   if(params['data']== "" || params['time']==""){
