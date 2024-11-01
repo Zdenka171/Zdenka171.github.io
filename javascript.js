2156eae8023669f97559cc0d1688f24c20fe4aae
Zdenka Plskova <plskova17@post.cz>
2024-11-01 11:51:45 +0100

    added EB Barcode API into html and main js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-EB.ScreenOrientation.leftHanded();
+// EB.ScreenOrientation.leftHanded();
 
 function submit() {
     var newData = document.createElement("div");
@@ -33,27 +33,26 @@ function showPosition(position) {
     "Longitude: " + position.coords.longitude;
 }
 
+function scanReceived(params){
+  // No data or no timestamp, scan failed.
+  if(params['data']== "" || params['time']==""){
+      document.getElementById('display').innerHTML = "Failed!";
+      return;
+  }
+  // Data and timestamp exist, barcode successful, show results
+  var displayStr = "Barcode Data: " + params['data']+"<br>Time: "+params['time'];
+  document.getElementById("display").innerHTML = displayStr;
+}
+
+function enableScanners(){
+  EB.Barcode.enable({}, scanReceived);
+  // Empty property hash, '{}' loads default values for the scanner.
+}
+
+function unloadEvent(){
+  EB.Barcode.disable();
+  // Disable Barcode on unload of page to free it up for other operations.
+}
 
 
 
-  // EB.Barcode.enable({}, scanReceived);
-       // function scanReceived(params){
-            // No data or no timestamp, scan failed.
-         //   if(params['data']== "" || params['time']==""){
-           //     document.getElementById('display').innerHTML = "Failed!";
-             //   return;
-            // }
-            // Data and timestamp exist, barcode successful, show results
-          //  var displayStr = "Barcode Data: " + params['data']+"<br>Time: "+params['time'];
-           // document.getElementById("display").innerHTML = displayStr;
-        //}
-
-        //function enableScanners(){
-            
-            // Empty property hash, '{}' loads default values for the scanner.
-        //}
-
-       //function unloadEvent(){
-        //EB.Barcode.disable();
-            // Disable Barcode on unload of page to free it up for other operations.
-      //}
